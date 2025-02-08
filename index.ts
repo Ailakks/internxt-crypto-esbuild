c408bca4e6e7fab7edd1c5bbea36ccd523417687
Ailakks <hola@ailakks.dev>
2025-02-08 18:08:03 +0100

    fix: crypto keygen

--- index.ts
+++ index.ts
@@ -1,5 +1,7 @@
 import CryptoJS from 'crypto-js';
-import { aes } from '@internxt/lib';
+import {aes} from '@internxt/lib';
+import {Auth, type CryptoProvider} from "@internxt/sdk";
+import kemBuilder from '@dashlane/pqc-kem-kyber512-browser';
 
 export function getAesInitFromEnv(): { iv: string; salt: string } {
     return { iv: "d139cb9a2cd17092e79e1861cf9d7023", salt: "38dce0391b49efba88dbc8c39ebf868f0267eb110bb0012ab27dc52a528d61b1d1ed9d76f400ff58e3240028442b1eab9bb84e111d9dadd997982dbde9dbd25e" };
@@ -13,6 +15,8 @@ export async function generateNewKeys(): Promise<{
     privateKeyArmored: string;
     publicKeyArmored: string;
     revocationCertificate: string;
+    publicKyberKeyBase64: string;
+    privateKyberKeyBase64: string;
 }> {
     const openpgp = await getOpenpgp();
 
@@ -21,10 +25,35 @@ export async function generateNewKeys(): Promise<{
         curve: 'ed25519',
     });
 
+    const kem = await kemBuilder();
+    const { publicKey: publicKyberKey, privateKey: privateKyberKey } = await kem.keypair();
+
     return {
         privateKeyArmored: privateKey,
         publicKeyArmored: Buffer.from(publicKey).toString('base64'),
         revocationCertificate: Buffer.from(revocationCertificate).toString('base64'),
+        publicKyberKeyBase64: Buffer.from(publicKyberKey).toString('base64'),
+        privateKyberKeyBase64: Buffer.from(privateKyberKey).toString('base64'),
+    };
+}
+
+export async function getKeys(password: string) {
+    const { privateKeyArmored, publicKeyArmored, revocationCertificate, publicKyberKeyBase64, privateKyberKeyBase64 } = await generateNewKeys();
+    const encPrivateKey = aes.encrypt(privateKeyArmored, password, getAesInitFromEnv());
+    const encPrivateKyberKey = aes.encrypt(privateKyberKeyBase64, password, getAesInitFromEnv());
+
+    return {
+        privateKeyEncrypted: encPrivateKey,
+        publicKey: publicKeyArmored,
+        revocationCertificate: revocationCertificate,
+        ecc: {
+            privateKeyEncrypted: encPrivateKey,
+            publicKey: publicKeyArmored,
+        },
+        kyber: {
+            publicKey: publicKyberKeyBase64,
+            privateKeyEncrypted: encPrivateKyberKey,
+        },
     };
 }
 
